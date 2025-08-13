9e8650d9f2a43a79e0bc6511c7846fd30e5bcee4
DJ Spiess <dj@deege.com>
2025-08-13 16:01:25 -0600

    Link add buttons to creation views

--- Sources/Features/Tasks/TasksListView.swift
+++ Sources/Features/Tasks/TasksListView.swift
@@ -5,6 +5,7 @@ import SwiftUI
 public struct TasksListView: View {
     public let home: HomeEntity
     @State private var tasks: [TaskEntity] = []
+    @State private var isPresentingNewTask = false
 
     public init(home: HomeEntity) { self.home = home }
 
@@ -15,7 +16,16 @@ public struct TasksListView: View {
         .navigationTitle("Tasks")
         .toolbar {
             ToolbarItem(placement: .navigationBarTrailing) {
-                Button(action: {}) { Image(systemName: "plus") }
+                Button {
+                    isPresentingNewTask = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+        }
+        .sheet(isPresented: $isPresentingNewTask) {
+            NavigationStack {
+                EditTaskView()
             }
         }
     }
